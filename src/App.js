4ec52b3a06880e7c2c1a65bc1ce69216bf8dd0dc
Anshul <anshul@socratease.co>
2022-07-21 22:00:08 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-import './App.css';
-import PitchDetect from './components/PitchDetect'
+import "./App.css";
+import { PitchDetector } from "https://esm.sh/pitchy@4";
 
 function App() {
+    const audioContext = new window.AudioContext();
+    const analyserNode = audioContext.createAnalyser();
 
-  const queryParams = new URLSearchParams(window.location.search);
-  const mode = queryParams.get("midi_file");
-  console.log(mode);
+    document.addEventListener("click", () => audioContext.resume());
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <PitchDetect/>
-      </header>
-    </div>
-  );
+    function updatePitch(analyserNode, detector, input, sampleRate) {
+        analyserNode.getFloatTimeDomainData(input);
+        const [pitch, clarity] = detector.findPitch(input, sampleRate);
+        console.log("pitch:" + pitch + "," + clarity);
+        window.setTimeout(() => updatePitch(analyserNode, detector, input, sampleRate), 100);
+    }
+
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+        audioContext.createMediaStreamSource(stream).connect(analyserNode);
+        const detector = PitchDetector.forFloat32Array(analyserNode.fftSize);
+        const input = new Float32Array(detector.inputLength);
+        updatePitch(analyserNode, detector, input, audioContext.sampleRate);
+    });
+
+    document.addEventListener("DOMContentLoaded", () => {
+        const audioContext = new window.AudioContext();
+        const analyserNode = audioContext.createAnalyser();
+
+        document.addEventListener("click", () => audioContext.resume());
+    });
+
+    const queryParams = new URLSearchParams(window.location.search);
+    const mode = queryParams.get("midi_file");
+    console.log(mode);
+
+    return (
+        <div className="App">
+            <header className="App-header">{"Under development"}</header>
+        </div>
+    );
 }
 
 export default App;
