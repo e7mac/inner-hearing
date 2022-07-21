2c9ea3af3a5c1ec2ba94fb9fb01981fae46504c1
Mayank Sanganeria <mayank10@fb.com>
2022-07-21 00:55:19 -0700

    js pitch detection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,43 @@
 import './App.css';
+import { PitchDetector } from "https://esm.sh/pitchy@4";
 
 function App() {
+
+
+  const audioContext = new window.AudioContext();
+  const analyserNode = audioContext.createAnalyser();
+
+  document
+    .addEventListener("click", () => audioContext.resume());
+
+  function updatePitch(analyserNode, detector, input, sampleRate) {
+    analyserNode.getFloatTimeDomainData(input);
+    const [pitch, clarity] = detector.findPitch(input, sampleRate);
+    console.log("pitch:" + pitch + "," + clarity);
+    window.setTimeout(
+      () => updatePitch(analyserNode, detector, input, sampleRate),
+      100
+    );
+  }
+
+  navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+    audioContext.createMediaStreamSource(stream).connect(analyserNode);
+    const detector = PitchDetector.forFloat32Array(analyserNode.fftSize);
+    const input = new Float32Array(detector.inputLength);
+    updatePitch(analyserNode, detector, input, audioContext.sampleRate);
+  });
+
+
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const audioContext = new window.AudioContext();
+    const analyserNode = audioContext.createAnalyser();
+
+    document
+      .addEventListener("click", () => audioContext.resume());
+
+  });
+
 
   const queryParams = new URLSearchParams(window.location.search);
   const mode = queryParams.get("midi_file");
